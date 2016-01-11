2b9f7b389ecc4ef3e2fbd8c20e5f19dfef672bc5
Mars Liu <march.liu@gmail.com>
2016-01-11 12:04:39 +0800

    fixed end of line error in advance

--- advance/src/java/com/dwarfartisan/parsec/EndOfLine.java
+++ advance/src/java/com/dwarfartisan/parsec/EndOfLine.java
@@ -7,12 +7,11 @@ import java.io.EOFException;
  * EndOfLine 尝试匹配 \n\r 或 \n
  */
 public class EndOfLine implements Parsec<String, Character> {
-
     @Override
     public String parse(State<Character> s) throws EOFException, ParsecException {
         new Ch('\n').parse(s);
         try{
-            new Ch('\r').parse(s);
+            new Try<>(new Ch('\r')).parse(s);
             return "\n\r";
         } catch (EOFException|ParsecException e) {
             return "\n";
