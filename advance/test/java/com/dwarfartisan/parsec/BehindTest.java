e60115e167de19b4bc6c71d607dd5a27044f9f09
Mars Liu <march.liu@gmail.com>
2016-09-16 00:45:32 +0800

    add a failed test
    add a test should failed about behind.

--- advance/test/java/com/dwarfartisan/parsec/BehindTest.java
+++ advance/test/java/com/dwarfartisan/parsec/BehindTest.java
@@ -2,7 +2,9 @@ package com.dwarfartisan.parsec;
 
 import static com.dwarfartisan.parsec.Txt.space;
 import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -34,5 +36,20 @@ public class BehindTest extends Base {
 
         assertEquals(re, "is");
         assertThat("Expect status stop after this (5) but is. ", state.status(), IsEqual.equalTo(5));
+    }
+
+    @Test
+    public void fail() throws Exception{
+        State<Character, Integer, Integer> state = newState("this is a string.");
+        Parsec<String, Character, Integer, Integer> parser =
+                new Text<Integer, Integer>("this").then(space()).then(new Behind<>(new Text<>(" is")));
+
+        try {
+            String re = parser.parse(state);
+        } catch (Exception e) {
+            assertThat("Expect status stop after this (5) but is. ", state.status(), IsEqual.equalTo(5));
+            assertThat("Expect the parser fail when try match \" is\"",
+                    e, IsInstanceOf.instanceOf(ParsecException.class));
+        }
     }
 }
